be1be5feb155a320a672b2e378c0fa4e933b969a
lanjudev16 <lanjudev16@gmail.com>
2023-09-01 17:50:17 +0600

    Redux toolkit setup done branch lanju

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,15 +1,14 @@
 import {
     createBrowserRouter,
 } from "react-router-dom";
+import LoginPage from "../Pages/LoginPage/LoginPage";
 export const router = createBrowserRouter([
     {
         path:"/",
-        element:<div>Hello</div>,
-        children:[
-            {
-                path:"/login",
-                element:<div>Login page</div>
-            }
-        ]
+        element:<div>Hello</div>
+    },
+    {
+        path:"/login",
+        element:<LoginPage></LoginPage>
     }
 ]);
