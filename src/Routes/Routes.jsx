abefa445db7a55489bd45e3075df6cf1dc28f1c7
jawad Bin Azam <abidibnazam@gmail.com>
2023-09-16 10:52:05 +0600

    working on calendar

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,19 +1,22 @@
-import {
-    createBrowserRouter,
-} from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import LoginPage from "../Pages/LoginPage/LoginPage";
 import ForgotPassword from "../Pages/ForgotPassword/ForgotPassword";
+import Calendar from "../Pages/Calendar/Calendar";
 export const router = createBrowserRouter([
-    {
-        path:"/",
-        element:<div>Hello</div>
-    },
-    {
-        path:"/login",
-        element:<LoginPage></LoginPage>
-    },
-    {
-        path:'/forgetPassword',
-        element:<ForgotPassword></ForgotPassword>
-    }
+  {
+    path: "/",
+    element: <div>Hello</div>,
+  },
+  {
+    path: "/login",
+    element: <LoginPage></LoginPage>,
+  },
+  {
+    path: "/forgetPassword",
+    element: <ForgotPassword></ForgotPassword>,
+  },
+  {
+    path: "/calendar",
+    element: <Calendar>s</Calendar>,
+  },
 ]);
