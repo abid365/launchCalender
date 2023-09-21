272a7ac7599ab396a418f66ae97ac81b7ee1c941
jawad Bin Azam <abidibnazam@gmail.com>
2023-09-21 18:40:42 +0600

    form setup done

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,4 +1,6 @@
+import { info } from "autoprefixer";
 import React, { useContext } from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   // const { user, setUser } = useContext(CalendarContext);
@@ -9,12 +11,12 @@ const Home = () => {
     const date = form.date.value;
     const launchType = form.launchType.value;
     const ads = form.ads.value;
-    const drone = form.drone.value;
+    const course = form.aboutCourse.value;
     const info = {
       date,
       launchType,
       ads,
-      drone,
+      course,
     };
     // setUser(info);
     console.log(info);
@@ -22,7 +24,7 @@ const Home = () => {
   return (
     <div>
       {/* Upper Secton */}
-      <div className="border border-red-400 min-h-screen py-10 text-[#ff6081]">
+      <div className="border border-[#a15999] min-h-screen py-10 text-[#a15999]">
         <h1 className="text-4xl font-semibold text-center py-2">
           Launch Calendar
         </h1>
@@ -37,7 +39,7 @@ const Home = () => {
               When are you going to launch the course? * <br />
             </label>
             {/* first select menu */}
-            <button className="px-3 py-2 rounded-lg border border-[#ff6081] text-xs my-3">
+            <button className="px-3 py-2 rounded-lg border border-[#a15999] text-xs my-3">
               Choose a date from this calendar{" "}
               <input className="bg-transparent" type="date" name="date" />
             </button>
@@ -47,7 +49,7 @@ const Home = () => {
               What type of launch is this ? <br />
             </label>
             <select
-              className="px-4 py-2 text-xs rounded-lg my-4 border border-[#ff6081] bg-transparent"
+              className="px-4 py-2 text-xs rounded-lg my-4 border border-[#a15999] bg-transparent"
               name="launchType"
               id=""
             >
@@ -76,51 +78,63 @@ const Home = () => {
             </span>
             <br />
             {/* course state */}
-            <label className="mt-10 text-sm block" htmlFor="courseState">
+            <label className="mt-10 text-md block" htmlFor="courseState">
               Is your course live already?
             </label>
             <br />
-            {/* <div className="flex flex-col justify-center items-center">
-              <div>
-                <input type="radio" name="courseState" />
-                <label className="px-1 text-xs text-left" htmlFor="yesRedy">
-                  Yes, it's ready to go
-                </label>{" "}
-              </div>
-              <br />
-              <div>
-                <input type="radio" name="courseState" />
-                <label className="px-1 text-xs text-left" htmlFor="notYet">
-                  Not yet, I'm going to finish setting up the course before the{" "}
-                  <br />
-                  launch goes live
-                </label>
-              </div>
-              <br />
-              <div>
-                <input type="radio" name="preSell" />
-                <label className="text-xs px-1 text-left" htmlFor="preSell">
-                  No, it's not built yet. I'm planning to pre-sell it before
-                  actually building the course
-                </label>
-              </div>
-            </div> */}
-            <div className="flex flex-col items-center border border-emerald-400 w-fit">
-              <div>
-                <input type="radio" id="huey" name="drone" value="huey" />
-                <label htmlFor="huey">Huey</label>
-              </div>
-              <div>
-                <input type="radio" id="dewey" name="drone" value="dewey" />
-                <label htmlFor="dewey">Dewey</label>
-              </div>
-              <div>
-                <input type="radio" id="louie" name="drone" value="louie" />
-                <label htmlFor="louie">Louie</label>
+            <div className="flex flex-col items-center">
+              <div className="flex flex-col items-start">
+                <div>
+                  <input
+                    type="radio"
+                    id="huey"
+                    name="aboutCourse"
+                    value="Yes, it's ready to go"
+                  />
+                  <label className="text-sm px-1" htmlFor="huey">
+                    Yes, it's ready to go
+                  </label>
+                </div>
+                <div>
+                  <input
+                    type="radio"
+                    id="dewey"
+                    name="aboutCourse"
+                    value="Not yet, I'm going to finish setting up the course before the launch goes live
+                    "
+                  />
+                  <label className="px-1 text-sm" htmlFor="dewey">
+                    Not yet, I'm going to finish setting up the course before
+                    the launch goes live
+                  </label>
+                </div>
+                <div>
+                  <input
+                    type="radio"
+                    id="louie"
+                    name="aboutCourse"
+                    value="No, it's not built yet. I'm planning to pre-sell it before actually building the course"
+                  />
+                  <label className="text-sm px-1" htmlFor="louie">
+                    No, it's not built yet. I'm planning to pre-sell it before
+                    actually building the course
+                  </label>
+                </div>
               </div>
             </div>
-            <div className="form-control mt-6">
-              <button className="btn">Sign Up</button>
+            <div className="form-control mt-10 flex flex-col items-center">
+              {/* <Link to={"/calendar"}>
+                {handleSubmit ? (
+                  <button className="btn">Sign Up</button>
+                ) : (
+                  <button disabled className="btn">
+                    Sign Up
+                  </button>
+                )}
+              </Link> */}
+              <button className="w-fit text-white px-5 py-2 rounded-md text-sm font-semibold tracking-tight bg-[#a15999] hover:bg-[#ff3266]">
+                Submit
+              </button>
             </div>
           </form>
         </div>
--- src/shared/Navbar.jsx
+++ src/shared/Navbar.jsx
@@ -3,10 +3,10 @@ import React from "react";
 const Navbar = () => {
   return (
     <div>
-      <nav className="border border-red-400 flex items-center justify-between px-7 py-4">
+      <nav className="border border-[#a15999] flex items-center justify-between px-7 py-4">
         <div>Nusrat Geek</div>
         <div>
-          <button className="border px-5 py-2 rounded-lg text-red-400 border-red-400">
+          <button className="border px-5 py-2 rounded-lg text-[#a15999] border-[#a15999]">
             Login
           </button>
         </div>
