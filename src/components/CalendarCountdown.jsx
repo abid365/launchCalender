dc51c00a539ece7fa19d239135e27bf5a423b164
jawad Bin Azam <abidibnazam@gmail.com>
2023-09-27 12:05:23 +0600

    calendar tweak

--- src/components/CalendarCountdown.jsx
+++ src/components/CalendarCountdown.jsx
@@ -1,139 +1,129 @@
 import React, { useState } from "react";
 
-function CalendarCountdown() {
-  const [launchDate, setLaunchDate] = useState("");
-  const [calendarContent, setCalendarContent] = useState([]);
+const CalendarCountdown = () => {
+  function renderCalendar() {
+    const launchDateInput = JSON.parse(localStorage.getItem("data"));
+    const calendarBody = document.getElementById("calendar-body");
+    const monthName = document.getElementById("month-name");
 
-  const tasks = [
-    "Create course outline",
-    "Record course videos",
-    "Edit course content",
-    "Set up eKajabi platform",
-    "Create marketing materials",
-    "Promote the course",
-    "Engage with students",
-    "Finalize course materials",
-    "Launch the course",
-    "Monitor course progress",
-  ];
+    // Get the launch date from the user input
+    const launchDate = new Date(launchDateInput);
+    console.log(launchDate);
 
-  const renderCalendar = () => {
-    const selectedDate = new Date(launchDate);
+    // Get the current date
     const currentDate = new Date();
-    let calendarRows = []; // Initialize as an empty array
 
-    while (currentDate <= selectedDate) {
+    // Clear the existing calendar content
+    calendarBody.innerHTML = "";
+
+    // Display the month name above the calendar
+    const monthText = launchDate.toLocaleDateString("en-US", {
+      month: "long",
+      year: "numeric",
+    });
+    monthName.textContent = monthText;
+
+    // Initialize an array to store the tasks
+    const tasks = [
+      "Create course outline",
+      "Record course videos",
+      "Edit course content",
+      "Set up eKajabi platform",
+      "Create marketing materials",
+      "Promote the course",
+      "Engage with students",
+      "Finalize course materials",
+      "Launch the course",
+      "Monitor course progress",
+    ];
+
+    // Create an empty table row
+    let currentRow = document.createElement("tr");
+
+    // Loop through the dates and create table cells with tasks from current date to launch date
+    while (currentDate <= launchDate) {
       const dayOfWeek = currentDate.getDay(); // 0 (Sun) to 6 (Sat)
+      const cell = document.createElement("td");
+      const taskContainer = document.createElement("div");
+      taskContainer.className = "task-container";
+      const dateText = document.createElement("div");
+      dateText.className = "date";
+      dateText.textContent = currentDate.getDate();
+
+      // Display the day name on each day
+      const dayNameText = document.createElement("div");
+      dayNameText.className = "day-name";
+      dayNameText.textContent = [
+        "Sun",
+        "Mon",
+        "Tue",
+        "Wed",
+        "Thu",
+        "Fri",
+        "Sat",
+      ][dayOfWeek];
+
+      // Calculate the task index based on the date difference
       const taskIndex = Math.min(
         Math.max(
           0,
-          Math.floor((selectedDate - new Date()) / (24 * 60 * 60 * 1000))
+          Math.floor((launchDate - new Date()) / (24 * 60 * 60 * 1000))
         ),
         tasks.length - 1
       );
+      const taskText = document.createElement("div");
+      taskText.className = "task";
+      taskText.textContent = tasks[taskIndex];
 
-      const dateCell = (
-        <td key={currentDate}>
-          <div className="task-container bg-gray-100 border p-2">
-            <div className="text-sm text-left pb-1">
-              {currentDate.getDate()}
-            </div>
-            <div className="text-sm text-center">{tasks[taskIndex]}</div>
-          </div>
-        </td>
-      );
+      taskContainer.appendChild(dateText);
+      taskContainer.appendChild(dayNameText); // Add the day name
+      taskContainer.appendChild(taskText);
+      cell.appendChild(taskContainer);
 
+      // Check if it's a Sunday and move to the next row
       if (dayOfWeek === 0) {
-        calendarRows.push(<tr key={currentDate}>{dateCell}</tr>);
-      } else {
-        // Check if the last array element exists and initialize if needed
-        if (!calendarRows[calendarRows.length - 1]) {
-          calendarRows[calendarRows.length - 1] = [];
-        }
-        calendarRows[calendarRows.length - 1].push(dateCell);
+        calendarBody.appendChild(currentRow);
+        currentRow = document.createElement("tr");
       }
 
+      currentRow.appendChild(cell);
       currentDate.setDate(currentDate.getDate() + 1);
     }
 
-    // Add 10 more days after the launch date
-    for (let i = 0; i < 10; i++) {
-      const dayOfWeek = currentDate.getDay(); // 0 (Sun) to 6 (Sat)
-      const dateCell = (
-        <td key={currentDate}>
-          <div className="task-container bg-gray-100 border p-2">
-            <div className="text-sm text-left pb-1">
-              {currentDate.getDate()}
-            </div>
-          </div>
-        </td>
-      );
-
-      if (dayOfWeek === 0) {
-        calendarRows.push(<tr key={currentDate}>{dateCell}</tr>);
-      } else {
-        // Check if the last array element exists and initialize if needed
-        if (!calendarRows[calendarRows.length - 1]) {
-          calendarRows[calendarRows.length - 1] = [];
-        }
-        calendarRows[calendarRows.length - 1].push(dateCell);
-      }
-
-      currentDate.setDate(currentDate.getDate() + 1);
+    // Append the last row if it's not a full week
+    if (currentRow.children.length > 0) {
+      calendarBody.appendChild(currentRow);
     }
+  }
 
-    setCalendarContent(calendarRows);
+  // end of renderCalendar function
+
+  // todo: this showUi fn shows the calendar after 2sec
+
+  const [loader, setLoader] = useState(true);
+
+  const showUi = () => {
+    if (loader) {
+      setTimeout(() => {
+        return <h1>Please wait..</h1>;
+      }, 2000);
+
+      setLoader(false);
+      onload(renderCalendar());
+    }
   };
 
   return (
     <div>
-      <h1 className="text-center text-2xl font-bold mt-4 mb-2">
-        Countdown to Launch in Calendar Format
-      </h1>
-      <label htmlFor="launchDate" className="block font-bold">
-        Enter Launch Date:
-      </label>
-      <input
-        type="date"
-        id="launchDate"
-        value={launchDate}
-        onChange={(e) => setLaunchDate(e.target.value)}
-        className="p-2 border border-gray-300 rounded mt-1 mb-2"
-      />
-      <button
-        onClick={renderCalendar}
-        className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
-      >
-        Render Calendar
-      </button>
-      <div id="calendar-container" className="mt-4">
-        <div id="month-name" className="font-bold text-xl mb-2">
-          {launchDate &&
-            new Date(launchDate).toLocaleDateString("en-US", {
-              month: "long",
-              year: "numeric",
-            })}
-        </div>
-        <table
-          id="calendar"
-          className="w-full border-collapse border border-gray-300"
-        >
-          <thead>
-            <tr>
-              <th className="bg-gray-200 text-center p-2">Sun</th>
-              <th className="bg-gray-200 text-center p-2">Mon</th>
-              <th className="bg-gray-200 text-center p-2">Tue</th>
-              <th className="bg-gray-200 text-center p-2">Wed</th>
-              <th className="bg-gray-200 text-center p-2">Thu</th>
-              <th className="bg-gray-200 text-center p-2">Fri</th>
-              <th className="bg-gray-200 text-center p-2">Sat</th>
-            </tr>
-          </thead>
-          <tbody>{calendarContent}</tbody>
+      <h1 className="text-center">Countdown to Launch in Calendar Format</h1>
+      <div id="calendar-container">
+        <div id="month-name"></div>
+        <table id="calendar">
+          <tbody id="calendar-body"></tbody>
         </table>
       </div>
     </div>
   );
-}
+};
 
 export default CalendarCountdown;
